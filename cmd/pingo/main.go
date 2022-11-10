273c6e850c9201b783657126ddc40469fce406e5
crims1n <pyromoosenac@gmail.com>
2022-11-09 23:59:32 -0500

    added functionality to obtain client IP address

--- cmd/pingo/main.go
+++ cmd/pingo/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("hi")
+	ip := GetLocalAddr()
+	fmt.Println(ip)
+}
+
+func GetLocalAddr() *net.IPNet {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, a := range addrs {
+		addr, ok := a.(*net.IPNet) // type assertion to get the actual IP pointer
+		isV4 := addr.IP.To4() != nil
+		if ok && !addr.IP.IsLoopback() && isV4 { // if assertion succeeds, IP isn't loopback, and IP isn't a v6 address
+			return addr
+		}
+	}
+	return nil
 }
